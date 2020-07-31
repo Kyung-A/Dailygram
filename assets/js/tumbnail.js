287b397a43f05570c2799dd4497f4b05d57c1dec
KyungA <nek1717@naver.com>
2020-08-01 02:24:30 +0900

    insta file

---
+++ assets/js/tumbnail.js
@@ -0,0 +1,12 @@
+const imgUpload = document.getElementById("jsuploadFile");
+
+imgUpload.onchange = function () {
+  let fileList = imgUpload.files;
+
+  let reader = new FileReader();
+  reader.readAsDataURL(fileList[0]);
+
+  reader.onload = function () {
+    document.querySelector("#jsTumbnail").src = reader.result;
+  };
+};
